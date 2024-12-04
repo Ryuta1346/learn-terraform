0b407def0c2258c5d9c4c291254e93ca541c38e9
Ryuta Tashiro <ryuta.tashiro@gmail.com>
2024-12-04 22:02:31 +0900

    feat: update dependencies in dev environment for visitor and company chat services to improve module references

--- realtime-chat/envs/dev/main.tf
+++ realtime-chat/envs/dev/main.tf
@@ -6,10 +6,10 @@ module "visitor_chat_network" {
 }
 
 module "visitor_chat_service" {
-  depends_on                  = [module.network]
+  depends_on                  = [module.visitor_chat_network]
   source                      = "../../services/visitor"
-  visitor_chat_vpc_id         = module.network.visitor_chat_vpc_id
-  visitor_chat_public_subnets = module.network.visitor_chat_public_subnets
+  visitor_chat_vpc_id         = module.visitor_chat_network.vpc_id
+  visitor_chat_public_subnets = module.visitor_chat_network.public_subnets
   environment                 = "dev"
 }
 
@@ -18,4 +18,12 @@ module "company_chat_network" {
   vpc_cidr_block     = var.company_vpc_cidr_block
   availability_zones = var.availability_zones
   environment        = "dev"
+}
+
+module "company_chat_service" {
+  depends_on                  = [module.company_chat_network]
+  source                      = "../../services/visitor"
+  visitor_chat_vpc_id         = module.company_chat_network.vpc_id
+  visitor_chat_public_subnets = module.company_chat_network.public_subnets
+  environment                 = "dev"
 }
