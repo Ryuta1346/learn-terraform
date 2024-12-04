cd5cfb6fee305fbf87c27808466783962dcda12b
Ryuta Tashiro <ryuta.tashiro@gmail.com>
2024-12-04 23:56:46 +0900

    feat: remove redundant description from public ALB security group module for clarity

--- realtime-chat/services/visitor/main.tf
+++ realtime-chat/services/visitor/main.tf
@@ -39,7 +39,6 @@ module "public_alb_sg" {
   vpc_id              = module.vpc.vpc_id
   environment         = var.environment
   project_name        = var.project_name
-  description         = "Security group for ${security_group_name}"
 }
 
 
