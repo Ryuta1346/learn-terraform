7b179271e7ea480d1d89a9a44c89c99f9925c95b
Ryuta Tashiro <ryuta.tashiro@gmail.com>
2024-12-15 16:13:02 +0900

    refactor: simplify ECS cluster resource definitions and remove unnecessary for_each loops

--- realtime-chat/modules/ecs/main.tf
+++ realtime-chat/modules/ecs/main.tf
@@ -1,7 +1,6 @@
 ## サービス・タスク定義の管理はecspressoで行うため、ここではクラスターのみ定義
 resource "aws_ecs_cluster" "cluster" {
-  for_each = { for ecs_cluster in var.ecs_cluster_vars : ecs_cluster.ecs_cluster_name => ecs_cluster }
-  name     = each.value.ecs_cluster_name
+  name     = var.ecs_cluster_vars.ecs_cluster_name
   setting {
     name  = "containerInsights"
     value = "enabled"
@@ -13,13 +12,12 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 resource "aws_ecs_cluster_capacity_providers" "cluster_capacity_provider" {
-  for_each           = { for ecs_cluster in var.ecs_cluster_vars : ecs_cluster.ecs_cluster_name => ecs_cluster }
-  cluster_name       = aws_ecs_cluster.cluster[each.key].name
-  capacity_providers = each.value.capacity_providers
+  cluster_name       = aws_ecs_cluster.cluster.name
+  capacity_providers = var.ecs_cluster_vars.capacity_providers
 
   default_capacity_provider_strategy {
-    base              = var.ecs_cluster_vars[each.key].default_base_count
-    weight            = var.ecs_cluster_vars[each.key].default_weight_count
-    capacity_provider = each.value.capacity_provider ? each.value.capacity_provider : var.ecs_cluster_vars[each.key].capacity_providers[0]
+    base              = var.ecs_cluster_vars.default_base_count
+    weight            = var.ecs_cluster_vars.default_weight_count
+    capacity_provider = var.ecs_cluster_vars.capacity_provider ? var.ecs_cluster_vars.capacity_provider : var.ecs_cluster_vars.capacity_providers[0]
   }
 }
