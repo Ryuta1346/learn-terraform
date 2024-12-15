50790266255e1b4be0ae873e234256e569f4319d
Ryuta Tashiro <ryuta.tashiro@gmail.com>
2024-12-16 08:09:05 +0900

    style: format Terraform files for consistency and readability

--- realtime-chat/envs/dev/main.tf
+++ realtime-chat/envs/dev/main.tf
@@ -17,34 +17,34 @@
 # }
 
 module "visitor_chat" {
-  source              = "../../services/visitor"
+  source = "../../services/visitor"
   # vpc_id              = module.vpc.vpc_id
-  vpc_cidr_block      = var.company_vpc_cidr_block
+  vpc_cidr_block = var.company_vpc_cidr_block
   # internet_gateway_id = module.internet_gateway.internet_gateway_id
-  availability_zones  = var.availability_zones
-  environment         = var.environment
-  project_name        = var.project_name
+  availability_zones = var.availability_zones
+  environment        = var.environment
+  project_name       = var.project_name
 }
 
 module "company_chat" {
-  source              = "../../services/company"
+  source = "../../services/company"
   # vpc_id              = module.vpc.vpc_id
-  vpc_cidr_block      = var.visitor_vpc_cidr_block
+  vpc_cidr_block = var.visitor_vpc_cidr_block
   # internet_gateway_id = module.internet_gateway.internet_gateway_id
-  availability_zones  = var.availability_zones
-  environment         = var.environment
-  project_name        = var.project_name
+  availability_zones = var.availability_zones
+  environment        = var.environment
+  project_name       = var.project_name
 }
 
 module "shared" {
-  depends_on          = [module.visitor_chat]
-  source              = "../../services/shared"
-  region              = "us-west-1"
+  depends_on = [module.visitor_chat]
+  source     = "../../services/shared"
+  region     = "us-west-1"
   # vpc_id              = module.vpc.vpc_id
-  vpc_cidr_block      = var.shared_vpc_cidr_block
+  vpc_cidr_block = var.shared_vpc_cidr_block
   # internet_gateway_id = module.internet_gateway.internet_gateway_id
-  visitor_chat_sg_id  = module.visitor_chat.visitor_ecs_sg_id
-  availability_zones  = var.availability_zones
-  environment         = var.environment
-  project_name        = var.project_name
+  visitor_chat_sg_id = module.visitor_chat.visitor_ecs_sg_id
+  availability_zones = var.availability_zones
+  environment        = var.environment
+  project_name       = var.project_name
 }
--- realtime-chat/envs/dev/terraform.tfvars
+++ realtime-chat/envs/dev/terraform.tfvars
@@ -2,7 +2,7 @@
 visitor_vpc_cidr_block = "10.0.0.0/20"
 company_vpc_cidr_block = "10.0.32.0/20"
 shared_vpc_cidr_block  = "10.0.64.0/20"
-availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-project_name       = "realtime-chat"
-region             = "us-east-1"
-environment        = "dev"
+availability_zones     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+project_name           = "realtime-chat"
+region                 = "us-east-1"
+environment            = "dev"
