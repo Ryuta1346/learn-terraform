52af240b7213dd3c40b4dc7b4a0e25beb129f01c
Ryuta Tashiro <ryuta.tashiro@gmail.com>
2024-12-15 13:47:12 +0900

    feat: add Terraform configuration for AWS backend and IAM role setup

---
+++ administrator/common/backend_setup.tf
@@ -0,0 +1,41 @@
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "learn-terraform-admin-state"
+
+  tags = {
+    Name = "learn-terraform-admin-state"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "tfstate_bucket" {
+  bucket = aws_s3_bucket.tfstate.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "tfstate_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.tfstate_bucket]
+  bucket     = aws_s3_bucket.tfstate.id
+  acl        = "private"
+}
+
+resource "aws_s3_bucket_versioning" "tfstate_bucket_versioning" {
+  bucket = aws_s3_bucket.tfstate.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_bucket_sse" {
+  bucket = aws_s3_bucket.tfstate.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.tfstate.id
+}
