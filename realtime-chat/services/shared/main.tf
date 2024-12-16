10a918395dd8ba848a478a3cf7b833e01d0f0d95
Ryuta Tashiro <ryuta.tashiro@gmail.com>
2024-12-16 09:30:20 +0900

    refactor: rename private_route_table module to private_ecs_route_table for clarity

--- realtime-chat/services/shared/main.tf
+++ realtime-chat/services/shared/main.tf
@@ -277,7 +277,7 @@ module "private_ecs_chat_sg" {
   project_name = var.project_name
 }
 
-module "private_route_table" {
+module "private_ecs_route_table" {
   source       = "../../modules/route_table"
   vpc_id       = module.vpc.vpc_id
   subnet_ids   = module.private_ecs_chat_subnet.subnet_ids
