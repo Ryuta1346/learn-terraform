6e8596331ebf430abe3380aca0df8c7ab355bdd1
Ryuta Tashiro <ryuta.tashiro@gmail.com>
2024-12-18 09:45:43 +0900

    fix: rename private ECS security group outputs and variables for consistency

--- realtime-chat/services/visitor/outputs.tf
+++ realtime-chat/services/visitor/outputs.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
   description = "The ID of the Visitor's Chat VPC"
 }
 
-output "cidr_block" {
+output "vpc_cidr_block" {
   value       = module.vpc.vpc_cidr_block
   description = "The CIDR block of the Visitor's Chat VPC"
 }
 
-output "private_ecs_route_table_id" {
+output "ecs_route_table_id" {
   value       = module.private_ecs_route_table.route_table_id
   description = "The ID of the private route table"
 }
 
-output "private_ecs_chat_sg" {
+output "ecs_chat_sg_id" {
   value       = module.private_ecs_chat_sg.sg_id
   description = "The ID of the private security group"
 }
