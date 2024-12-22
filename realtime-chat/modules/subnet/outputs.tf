c33812a8d498d5a5223faf1d1d586ede528df924
Ryuta Tashiro <ryuta.tashiro@gmail.com>
2024-12-22 18:38:01 +0900

    fix: remove unused subnet_names output from subnet module

--- realtime-chat/modules/subnet/outputs.tf
+++ realtime-chat/modules/subnet/outputs.tf
@@ -2,8 +2,3 @@ output "subnet_ids" {
   description = "The public subnets for the VPC"
   value       = [for subnet in aws_subnet.subnet : subnet.id]
 }
-
-output "subnet_names" {
-  description = "The public subnets for the VPC"
-  value       = [for subnet in aws_subnet.subnet : subnet.name]
-}
